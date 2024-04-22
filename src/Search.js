3020db2f150e5728a662065b43429aaa97b3273c
M4rti21 <m4rti21@proton.me>
2024-04-23 00:35:31 +0200

    ._.

--- src/Search.js
+++ src/Search.js
@@ -5,18 +5,27 @@ import { useDebouncedCallback } from "use-debounce";
 
 function Search() {
 
-    useEffect(() => {
-        StorageSearch("");
-    }, [])
+    const len = 24;
 
+    const [searchValue, setSearchValue] = useState("");
     const [storageRes, setStorageRes] = useState([]);
 
     const debounced = useDebouncedCallback(StorageSearch, 500);
 
+    useEffect(() => {
+        StorageSearch();
+    }, [])
+
     async function StorageSearch(value) {
-        const res = await fetch(`${env.apiEndpoint}/search?q=${value}`);
-        const data = await res.json();
-        setStorageRes(data.reverse());
+        if (value) setSearchValue(value);
+        try {
+            const res = await fetch(`${env.apiEndpoint}/search?q=${value || searchValue}&limit=${len}&offset=${storageRes.length}`);
+            const data = await res.json();
+            setStorageRes([...storageRes, ...data.reverse()]);
+        } catch (err) {
+            console.error(err);
+            setStorageRes([]);
+        }
     }
 
     function secondsToTime(secs) {
@@ -36,12 +45,10 @@ function Search() {
                     Back
                 </button>
             </Link>
-            <form onSubmit={StorageSearch} className="grow">
-                <label className="input input-bordered flex items-center gap-2">
-                    <input onChange={(e) => debounced(e.target.value)} type="text" className="grow" placeholder="Search" />
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path fillRule="evenodd" d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z" clipRule="evenodd" /></svg>
-                </label>
-            </form>
+            <label className="grow input input-bordered flex items-center gap-2">
+                <input onChange={(e) => debounced(e.target.value)} type="text" className="grow" placeholder="Search" />
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path fillRule="evenodd" d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z" clipRule="evenodd" /></svg>
+            </label>
         </div>
         <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
             {storageRes.map((song, i) => (
@@ -71,6 +78,11 @@ function Search() {
                 </div>
             ))}
         </div>
+        <button className="btn btn-primary"
+            disabled={storageRes.length % len != 0}
+            onClick={StorageSearch}>
+            Load More
+        </button>
     </>;
 }
 export default Search;
